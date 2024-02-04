bb3c745eaba570e1106f50d1f2b17b69e46ba484
sam <llh@birdpaper.com>
2024-02-04 14:39:42 +0800

    fonts

--- docs/src/views/pages/home/index.vue
+++ docs/src/views/pages/home/index.vue
@@ -5,15 +5,29 @@
     </div>
     <div :class="`${name}-search`">
       <div :class="`${name}-search-cotainer`">
-        <div :class="`${name}-search-inner`"></div>
+        <div :class="`${name}-search-inner`">
+          <!-- 类型选择器 -->
+          <div :class="`${name}-search-select`">
+            <div :class="`${name}-search-select-container`">
+              <span>All Icons</span>
+              <IconArrowDownSLine />
+            </div>
+          </div>
+
+          <div :class="`${name}-search-input`">
+            <div :class="`${name}-search-input-icon`"></div>
+            <div :class="`${name}-search-input-inner`">
+              <input type="text" name="" id="" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-// import { iconType } from "birdpaper-icon";
-// console.log('[ iconType ]-16', iconType);
+import { iconType, IconArrowDownSLine } from "birdpaper-icon";
 
 const name = "home-page";
 </script>
