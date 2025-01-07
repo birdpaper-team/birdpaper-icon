ab45c2963af27e98f9f1b32871b7383a4b1938e2
Sam <llh@birdpaper.com>
2025-01-07 09:20:52 +0800

    重构样式文件，迁移至 SCSS，添加新样式和变量，删除不必要的 LESS 文件

--- docs/src/main.ts
+++ docs/src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from "vue";
 import { router } from "./router/index";
 import App from "./App.vue";
 
-import "./style/index.less";
+import "uno.css";
+import "./scss/index.scss";
 
 import BirdpaperUI from "birdpaper-ui";
 import "birdpaper-ui/theme/src/index.css";
--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,28 +1,31 @@
 import { defineConfig } from "vite";
 const path = require("path");
 import vue from "@vitejs/plugin-vue";
-import VueSetupExtend from "vite-plugin-vue-setup-extend";
 import viteCompression from "vite-plugin-compression";
 
-export default defineConfig({
-  plugins: [
-    VueSetupExtend(),
-    vue({
-      script: {
-        defineModel: true,
+export default async () => {
+  const UnoCSS = (await import("unocss/vite")).default;
+
+  return defineConfig({
+    plugins: [
+      vue({
+        script: {
+          defineModel: true,
+        },
+      }),
+      UnoCSS(),
+      viteCompression({
+        verbose: true,
+        disable: false,
+        threshold: 10240,
+        algorithm: "gzip",
+        ext: ".gz",
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
-    }),
-    viteCompression({
-      verbose: true,
-      disable: false,
-      threshold: 10240,
-      algorithm: "gzip",
-      ext: ".gz",
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
     },
-  },
-});
+  });
+};
