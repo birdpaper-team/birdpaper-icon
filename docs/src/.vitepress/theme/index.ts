61b68ec550e225d6c2af83b1a680db9f0e4d2582
Sam <llh@birdpaper.com>
2025-01-08 15:50:50 +0800

    添加 SCSS 文件，重构样式结构，更新依赖，删除旧组件，增加新功能

--- docs/src/.vitepress/theme/index.ts
+++ docs/src/.vitepress/theme/index.ts
@@ -3,16 +3,21 @@ import Layout from "./layout.vue";
 import Theme from "vitepress/theme";
 import type { EnhanceAppContext } from "vitepress";
 
+import BirdpaperUI from "birdpaper-ui";
+import "birdpaper-ui/theme/src/index.css";
+
 import BirdpaperIcon from "birdpaper-icon";
 
 import "uno.css";
 import "../../style/index.scss";
+import "../../scss/index.scss";
 
 export default {
   ...Theme,
   Layout,
   extends: DefaultTheme,
   enhanceApp(ctx: EnhanceAppContext) {
+    ctx.app.use(BirdpaperUI);
     ctx.app.use(BirdpaperIcon);
   },
 };
