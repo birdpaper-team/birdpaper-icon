0c62601d791c9d872428643891f22c452201f9a1
Sam <llh@birdpaper.com>
2025-01-10 18:16:14 +0800

    新增 TypeScript 声明文件以支持各种资源类型，更新构建任务以包含打包逻辑

--- scripts/build/gulpfile.ts
+++ scripts/build/gulpfile.ts
@@ -1,4 +1,4 @@
 import { series, parallel } from "gulp";
 import { buildModules, buildBundle } from "./src/tasks";
 
-export default series(parallel(buildModules));
+export default series(parallel(buildModules), buildBundle);
