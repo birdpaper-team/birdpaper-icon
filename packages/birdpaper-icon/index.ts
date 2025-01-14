869f2e8d19f81a98cac7d38a4044340d16c56b8c
Sam <llh@birdpaper.com>
2025-01-14 17:28:50 +0800

    重命名图标生成脚本，更新依赖版本，移除不再使用的包，添加 tsconfig 配置

--- packages/birdpaper-icon/index.ts
+++ packages/birdpaper-icon/index.ts
@@ -1,7 +1,7 @@
 import installer from "./default";
 
-export * from "./installer";
 export * from "@birdpaper-icon/components/src";
+export * from "./installer";
 
 export const install = installer.install;
 export default installer;
