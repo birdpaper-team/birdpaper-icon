b15d5649d5b92aef5cbfde3fe48cfc61a9c683a7
Sam <llh@birdpaper.com>
2025-01-14 14:29:27 +0800

    调整构建任务顺序，将构建模块和打包任务并行执行

--- scripts/build/gulpfile.ts
+++ scripts/build/gulpfile.ts
@@ -1,4 +1,4 @@
 import { series, parallel } from "gulp";
 import { buildModules, buildBundle, copyFiles } from "./src/tasks";
 
-export default series(parallel(buildModules), buildBundle, copyFiles);
+export default series(parallel(buildModules, buildBundle), copyFiles);
