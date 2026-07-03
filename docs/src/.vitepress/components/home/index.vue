bba696ffcb3aab4ebaf85803aaf493c4041c4769
Sam <llh@birdpaper.com>
2026-07-03 09:49:33 +0800

    feat: 更新组件和配置，优化国际化支持及模块解析

--- docs/src/.vitepress/components/home/index.vue
+++ docs/src/.vitepress/components/home/index.vue
@@ -3,17 +3,17 @@
     <div :class="`${name}-banner`">
       <div :class="`${name}-banner-content`">
         <div :class="`${name}-banner-content-title`">
-          <p v-html="inrto[lang].title"></p>
+          <p v-html="inrto[locale].title"></p>
         </div>
         <div :class="`${name}-banner-content-remark`">
           <p>
-            <span v-html="inrto[lang].remark[0]" />
+            <span v-html="inrto[locale].remark[0]" />
             <img :src="isDark ? remixLogoWhite : remixLogo" @click="linkTo('remix')" />
-            <span v-html="inrto[lang].remark[1]" />
+            <span v-html="inrto[locale].remark[1]" />
           </p>
         </div>
         <div :class="`${name}-banner-content-option mt-20px`">
-          <bp-button>{{ inrto[lang].btn.quickStart }}</bp-button>
+          <bp-button>{{ inrto[locale].btn.quickStart }}</bp-button>
           <bp-button :icon="IconGithubFill" type="plain" status="primary" @click="linkTo('github')">Github</bp-button>
         </div>
       </div>
@@ -54,8 +54,11 @@ import remixLogoWhite from "../../../assets/remix-logo-white.svg";
 
 const name = "home-page";
 const { lang, isDark } = useData();
+const locale = computed<Locale>(() => (lang.value === "zh-CN" ? "zh-CN" : "en"));
 
-const inrto = {
+type Locale = "zh-CN" | "en";
+
+const inrto: Record<Locale, { title: string; remark: string[]; btn: { quickStart: string } }> = {
   "zh-CN": {
     title: `Vue3 图标组件库`,
     remark: [`这是一个基于优秀的开源图标 - `, `二次开发的 Vue3 组件库，仅供学习和参考使用, 感谢 Remix 设计团队。`],
@@ -81,7 +84,7 @@ const searchIcons = computed(() => {
   const rawIcons = JSON.parse(JSON.stringify(iconInfo));
 
   for (let i = 0; i < rawIcons.length; i++) {
-    rawIcons[i].list = rawIcons[i].list.filter((item) => item.includes(searchKey.value));
+    rawIcons[i].list = rawIcons[i].list.filter((item: string | string[]) => item.includes(searchKey.value));
   }
   return rawIcons;
 });
@@ -96,5 +99,5 @@ const homeUrlMap = {
   github: "https://github.com/birdpaper-team/birdpaper-icon",
   npm: "https://www.npmjs.com/package/birdpaper-icon",
 };
-const linkTo = (type: string) => window.open(homeUrlMap[type]);
+const linkTo = (type: keyof typeof homeUrlMap) => window.open(homeUrlMap[type]);
 </script>
